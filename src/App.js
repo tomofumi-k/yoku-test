7e36f319206a527956a6857f329e01c819878bea
tomofumi-k <t.k.20202210@gmail.com>
2023-01-07 19:45:52 +0900

    フォームに入力したテキストをリストとして表示/追加される

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import TodoList from "./B-test/TodoList";
+import { v4 as uuidv4 } from 'uuid';
 
 
 function App() {
-  const [todos, setTodos] = useState(["Todo1", "Todo2"]);
+  const [todos, setTodos] = useState([
+    { id: 1, name: "Todo1", completed: false },
+  ]);
+
+  const todoNameRef = useRef();
+
+  const handleAddTodo = (e) =>{
+    const name = todoNameRef.current.value;
+    setTodos((prevTodos) => {
+      return [...prevTodos, {id: uuidv4(), name:name, completed: false}];
+    })
+    todoNameRef.current.value = null;
+  };
+
   return (
     <>
-    <input type="text" />
+    <input type="text" ref={todoNameRef}/>
+    <button onClick={handleAddTodo}>追加</button>
     <TodoList todos={todos} />
     </>
   );
-}
+};
 
 export default App;
--- src/B-test/Todo.js
+++ src/B-test/Todo.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Todo = ({todo}) => {
   return (
-    <div>{todo}</div>
-    )}
+    <div>
+      <label>
+        <input type="checkbox" checked={todo.completed} readOnly />
+      </label>
+      {todo.name}
+    </div>
+    );
+  };
+    
 
     export default Todo;
