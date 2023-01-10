a37338782b2034fed5d5d1d69087949d4877f095
tomofumi-k <t.k.20202210@gmail.com>
2023-01-11 01:15:42 +0900

    雛形作成

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import React, { useState } from "react";
+import { List } from "./C-test/List";
+import "./C-test/c-test.scss";
+
 function App() {
-  return <div className="App"></div>;
+  const [fruits, setfruits] = useState([
+    { id: 1, name: "Apple" },
+    { id: 2, name: "grape" },
+    { id: 3, name: "Strawberry" },
+    { id: 4, name: "Cherry" },
+    { id: 5, name: "Plum" },
+    { id: 6, name: "Watermelon" },
+    { id: 7, name: "Banana" },
+    { id: 8, name: "Peach" },
+  ]);
+
+  return <List fruits={fruits} setfruits={setfruits} />;
 }
 
 export default App;
