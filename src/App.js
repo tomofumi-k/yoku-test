0a861fa97099eb02f0b75eaefce8a5ac10b6cfa3
tomofumi-k <t.k.20202210@gmail.com>
2023-01-11 01:18:35 +0900

    リセット

--- src/App.js
+++ src/App.js
@@ -1,20 +1,3 @@
-import React, { useState } from "react";
-import { List } from "./C-test/List";
-import "./C-test/c-test.scss";
-
-function App() {
-  const [fruits, setfruits] = useState([
-    { id: 1, name: "Apple" },
-    { id: 2, name: "grape" },
-    { id: 3, name: "Strawberry" },
-    { id: 4, name: "Cherry" },
-    { id: 5, name: "Plum" },
-    { id: 6, name: "Watermelon" },
-    { id: 7, name: "Banana" },
-    { id: 8, name: "Peach" },
-  ]);
-
-  return <List fruits={fruits} setfruits={setfruits} />;
-}
+function App() {}
 
 export default App;
--- src/C-test/List.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const List = (fruits) => {
-  return (
-    <ul className="c-list">
-      <li className="">{fruits.name}</li>
-    </ul>
-  );
-};
