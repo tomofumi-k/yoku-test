618ac031398b3b88152800fc10ba837398a6b7de
tomofumi-k <t.k.20202210@gmail.com>
2023-01-11 00:43:13 +0900

    フォームが空白の場合、追加できないように修正

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import "./B-test/b__section.scss";
 
 function App() {
   // フォームに入力された文字を監視して保管する箱
+  // 状態が変化するものはuseStateで宣言する
+  // taskListにはuseState(この中身に入ってくるもの、[]なら配列)
+  // setTaskListは状態を変更する関数
   const [taskList, setTaskList] = useState([
     { id: 1, text: "Apple" },
     { id: 2, text: "grape" },
@@ -12,7 +15,7 @@ function App() {
   ]);
 
   return (
-    // 二つのコンポーネントにusestateを返してる
+    // 二つのコンポーネントにusestateを渡してる
     <div className="bsection">
       <InputForm taskList={taskList} setTaskList={setTaskList} />
       <TodoList taskList={taskList} setTaskList={setTaskList} />
--- src/B-test/InputForm.jsx
+++ src/B-test/InputForm.jsx
@@ -1,21 +1,33 @@
 // 追加するフォームのコンポーネント
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+
+// app.jsの [taskList, setTaskList] を受け取る
 export const InputForm = ({ taskList, setTaskList }) => {
+  // taskList の中身を inputText に渡していく
   const [inputText, setInputText] = useState("");
 
+  // e.preventDefault は再レンダリングを阻止するために使用
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // 空白の場合,リターンする
+    if (inputText === "") return;
     setTaskList([
+      // ...がないと追加ではなく更新される。
+      // ...は以前の状態に追加する
       ...taskList,
       {
+        // taskList.length でもできるが、アンチパターンになるため uuid を使用
         id: uuidv4(),
         text: inputText,
       },
     ]);
+    // 入力した文字を削除
     setInputText("");
   };
 
+  // フォームに入力された文字を取得してる
   const handleChange = (e) => {
     setInputText(e.target.value);
   };
@@ -23,6 +35,7 @@ export const InputForm = ({ taskList, setTaskList }) => {
   return (
     <div>
       <form className="bsection__input--form" onSubmit={handleSubmit}>
+        {/* valueを inputText にすることで setInputText(""); のからの状態になる */}
         <input className="bsection__input" type="text" onChange={handleChange} value={inputText} />
         <button className="bsection__button">追加</button>
       </form>
