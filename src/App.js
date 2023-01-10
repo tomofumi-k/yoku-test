b2412c1c84590f9581ba3eed798d52cade9700d2
tomofumi-k <t.k.20202210@gmail.com>
2023-01-11 00:44:23 +0900

    コメント削除

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import { TodoList } from "./B-test/TodoList";
 import "./B-test/b__section.scss";
 
 function App() {
-  // フォームに入力された文字を監視して保管する箱
-  // 状態が変化するものはuseStateで宣言する
-  // taskListにはuseState(この中身に入ってくるもの、[]なら配列)
-  // setTaskListは状態を変更する関数
   const [taskList, setTaskList] = useState([
     { id: 1, text: "Apple" },
     { id: 2, text: "grape" },
@@ -15,7 +11,6 @@ function App() {
   ]);
 
   return (
-    // 二つのコンポーネントにusestateを渡してる
     <div className="bsection">
       <InputForm taskList={taskList} setTaskList={setTaskList} />
       <TodoList taskList={taskList} setTaskList={setTaskList} />
--- src/B-test/TodoList.jsx
+++ src/B-test/TodoList.jsx
@@ -1,24 +1,18 @@
-// 追加されるTodoのコンポーネント
 import React from "react";
 
 export const TodoList = ({ taskList, setTaskList }) => {
-  // Todoの削除
   const handleDelete = (id) => {
-    // filter((task) で taskを監視、task.id(todoリストのid) が id と = だった場合,除外する
     setTaskList(taskList.filter((task) => task.id !== id));
   };
 
   return (
     <>
-      {/* map関数で一つずつ取り出す。forEachみたいなもの */}
-      {/* 変化するものが task , index。これらが渡される */}
       {taskList.map((task, index) => (
         <div className="todo__input--container" key={index}>
           <label className="todo__deletebutton">
             <button onClick={() => handleDelete(task.id)}>x</button>
           </label>
           <div className="todoText">
-            {/* InputForm.jsxで定義したtextが出力される */}
             <span>{task.text}</span>
           </div>
         </div>
