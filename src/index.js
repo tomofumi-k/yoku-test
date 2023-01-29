d53139ddddefdf077f11f313c919273f2339be0d
tomofumi-k <t.k.20202210@gmail.com>
2023-01-29 17:18:29 +0900

    jsxに変更

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import AtestApp from "./A-test/AtestApp";
+import AtestApp from "./A-test/AtestApp";
 import BtestApp from "./B-test/BtestApp";
 import CtestApp from "./C-test/CtestApp";
 import "./base.scss";
@@ -8,8 +8,8 @@ import "./base.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <AtestApp /> */}
-    {/* <BtestApp /> */}
+    <AtestApp />
+    <BtestApp />
     <CtestApp />
   </React.StrictMode>
 );
